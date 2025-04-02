5b7232e8133a49311f49871c837fc30c04adaaf7
Cristian Muñoz <cristian.munoz3@udea.edu.co>
2025-04-02 12:40:07 -0500

    Se agrega la ejecución del comando de cat del proceso

--- lab01-psinfo/main.c
+++ lab01-psinfo/main.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 int main(int argc, char* argv[]){
     char inicio_comando[50] = "cat /proc/";
     strcat(inicio_comando, argv[1]);
     strcat(inicio_comando, "/status");
-    printf("%s\n", inicio_comando);
+    system(inicio_comando);
 }
 
