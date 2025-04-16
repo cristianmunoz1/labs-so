a323069079110aa3de8974ba252cb64fdf67defe
Cristian Muñoz <cristian.munoz3@udea.edu.co>
2025-04-15 22:24:05 -0500

    Se agregan comentarios a functions.c y al makefile

--- lab01-psinfo/version1/functions.c
+++ lab01-psinfo/version1/functions.c
@@ -83,58 +83,90 @@ void printProcessQueue(struct Node* head){
 
 
 int processPIDs(int argc, char* argv[], struct Node** queue, int *hasR, int *hasL) {
+    //Si hay menos de 2 argumentos, solo se ejecutó el comando y no se pasaron pids ni flags
     if (argc < 2) {
+        printf("\033[1;33m ------------------- Información de PSINFO --------------------\n\033[0m");
         printf("Uso: psinfo [ -l pid1 pid2 ... ] [ -r ]\n");
+        printf("\nPara ejemplos más detallados del comando, visita la man page del programa ejecutando \"man psinfo\"\n\n");
         return -1;
     }
-
+    //Variable contadora para las validaciones de parámetros
     int i = 1;
+
+    // Si el primer argumento es la flag -l entonces se pasa a hasL 1. Se aumenta el contador para validar los otros argumentos. 
+    //Opción donde se tiene -l entre los argumentos
     if (strcmp(argv[i], "-l") == 0) {
         *hasL = 1;
         i++;
+        
 
+        // Si i siendo 2 es mayor o igual que la cantidad de argumentos, significa que solo se pasó -l y no se pasaron pids. 
         if (i >= argc || strcmp(argv[i], "-r") == 0) {
-            printf("Error: Debe ingresar al menos un PID después de -l.\n");
+            printf("\033[1;31m ERROR. PARÁMETROS INCORRECTOS \033[0m\n");
+            printf("Después de la flag -l debe ir mínimo un pid. Ejemplo: psinfo -l pid1 pid2 ...\n\n");
+            printf("\033[1;33m Para más información acceda a la man page del comando ejecutando: man psinfo\033[0m\n");
             return -1;
         }
-
-        // Leer PIDs múltiples
+       
+        //Si llegó a este punto significa que hay más de 2 argumentos
+        //Si hay más de 2 argumentos se valida cada uno de ellos, esperando que todos excepto -r si existe, sean numéricos. Si alguno no lo es, retorna un error. 
         while (i < argc && strcmp(argv[i], "-r") != 0) {
             if (!isNumeric(argv[i])) {
-                printf("Error: PID '%s' no es válido. Debe ser numérico.\n", argv[i]);
+                printf("\033[1;31m ERROR. PARÁMETROS INCORRECTOS \033[0m\n");
+                printf("Todos los process ids deben ser numéricos. El parámetro que causa el error es: %s ...\n\n", argv[i]);
+                printf("\033[1;33m Para más información acceda a la man page del comando ejecutando: man psinfo\033[0m\n");
                 return -1;
             }
+
+            //Si no ha retornado error hasta el momento, entonces se guarda la info del proceso en la cola.
             storeProcessInfo(argv[i], queue);
+            //Se aumenta en uno para ir iterando sobre los argumentos. 
             i++;
         }
-
+        
+        // Si hay más argumentos, presumiblemente es la flag -r. Por lo tanto se verifica, si lo es, se pone hasR en 1
+        // Si resulta que en el ciclo anterior se salió sin terminar porque encontró la flag -r, entonces i estará antes de argc-1. Por lo tanto, se 
+        // lanza el error y se informa al usuario. 
         if (i < argc && strcmp(argv[i], "-r") == 0) {
             *hasR = 1;
             if (i != argc - 1) {
-                printf("Error: La bandera -r debe estar al final del comando.\n");
+                printf("\033[1;31m ERROR. PARÁMETROS INCORRECTOS \033[0m\n");
+                printf("La bandera -r debe estar al final del comando.\n");
+                printf("\033[1;33m Para más información acceda a la man page del comando ejecutando: man psinfo\033[0m\n");
+
                 return -1;
             }
         }
-
+    // Si el programa no tiene la flag -l
     } else {
-        // Modo sin -l (1 solo PID)
+        // Verifica que el pid sea numérico, si no lo es, salta un error. 
         if (!isNumeric(argv[i])) {
-            printf("Error: PID '%s' no es válido. Debe ser numérico.\n", argv[i]);
+            printf("\033[1;31m ERROR. PARÁMETROS INCORRECTOS \033[0m\n");
+            printf("PID '%s' no es válido. Debe ser numérico.\n", argv[i]);
+            printf("\033[1;33m Para más información acceda a la man page del comando ejecutando: man psinfo\033[0m\n");
             return -1;
         }
-
+        // Si no saltó el error, significa que el parámetro es correcto y se guarda la información del mismo en la cola. 
         storeProcessInfo(argv[i], queue);
+        //Aumentamos el contador para verificar si hay otros argumentos adicionales además del pid pero sin incluir -l
         i++;
-
+        // Si hay más argumentos pero no se pasó -l entonces hay 2 opciones, o es la flag -r, caso en el cual no debe lanzar error
+        // o son otros process ids, con lo cual si debe lanzar error. 
         if (i < argc) {
+            //Si el argumento coincide con la flag -r se pone hasR en 1
             if (strcmp(argv[i], "-r") == 0) {
                 *hasR = 1;
+                //Si no es el último argumento, significa que -r no está al final y debe lanzar un error. 
                 if (i != argc - 1) {
                     printf("Error: La bandera -r debe estar al final del comando.\n");
                     return -1;
                 }
+                //Si está de último, el código continúa normalmente.
+            //Si no es la flag -r significa que el usuario está tratando de pasar otros pids sin la flag -l, por lo que debe saltar un error. 
             } else {
-                printf("Error: Argumento no reconocido: %s\n", argv[i]);
+                printf("\033[1;31m ERROR. PARÁMETROS INCORRECTOS \033[0m\n");
+                printf("Si quiere pasar múltiples process id, debe poner la flag -l. Ejemplo: psinfo -l pid1 pid2 ...\n\n");
+                printf("\033[1;33m Para más información acceda a la man page del comando ejecutando: man psinfo\033[0m\n");
                 return -1;
             }
         }
@@ -143,45 +175,57 @@ int processPIDs(int argc, char* argv[], struct Node** queue, int *hasR, int *has
     return 0;
 }
 
+// Función para encontrar la primera posición del argumento que corresponde a un process id
 int firstProcessPosition(int argc, char* argv[]){
+    //Iteramos sobre todos los argumentos
     for (int i = 1; i<argc; i++){
+        // Valida si cada argumento es igual a -l o a -r. Si no es así, entonces retorna la posición, el i. 
         if(!((strcmp(argv[i], "-l") == 0) || (strcmp(argv[i], "-r") == 0))){
-            printf("La posición del primer argumento proceso es: %d\n", i);
             return i;
         }
     }
-
+    // Si no retornó nada, entonces retorna un error, ya que no habría procesos en los argumentos
     return -1;
 }
 
+//Función para escribir un reporte en un archivo .info
 void writeReport(int argc, char* argv[], struct Node* head, int firstProcessPosition){
+    //Declara el nombre del archivo como report. El cual es un apuntador a un tipo FILE de C. 
     FILE *report;
+    //Inicializamos el arreglo de caracteres como psinfo-report- que es el prefijo de todos los archivos .info y donde concatenaremos los pids.
     char nombre_archivo[128] = "psinfo-report-";
+    // Creamos un nodo temporal que apunta a la cabeza de la cola para tener la info del primer pid. 
     struct Node* temp = head;
 
+    //Iteramos los argumentos del comando desde la posición del primer argumento que corresponde a un process id. 
     for (int i = firstProcessPosition; i < argc; i++) {
-        // Ignorar banderas como -l o -r
+        // Si el argumento corresponde a -l o -r entonces los ignoramos y continuamos. 
         if (strcmp(argv[i], "-r") == 0 || strcmp(argv[i], "-l") == 0) {
             continue;
         }
 
         strcat(nombre_archivo, argv[i]);
-
         // Solo añadir '-' si no es el último PID (verificamos si hay más PIDs adelante)
+        // Tomamos la j como el argumento siguiente al que estamos verificando
         int j = i + 1;
+        // Mientras el j no sea el último argumento y igual a -l o -r entonces que continúe, aumentamos el contador. 
         while (j < argc && (strcmp(argv[j], "-r") == 0 || strcmp(argv[j], "-l") == 0)){
             j++;
         }
-
+        // Si el j no es todavía el último argumento, que debería ser -r, entonces se agrega el -
         if (j < argc) {
             strcat(nombre_archivo, "-");
+        // Si es ya el último, entonces es -r, no agregamos nada sino que le ponemos la extensión .info y salimos del for. 
         } else {
             strcat(nombre_archivo, ".info");
             break;
         }
     }
-
+    
+    // Abrimos el archivo con fopen en modo escritura
     report = fopen(nombre_archivo, "w");
+    // Mientras el nodo temporal tenga datos, osea, no sea nulo, (que no haya llegado al final de la cola) entonces que con fprintf ponga la info 
+    // del proceso actual en el archivo y avance hacia el nodo siguiente. 
     while (temp != NULL){
         fprintf(report, "%s\n", temp->data);
         temp = temp->next;
