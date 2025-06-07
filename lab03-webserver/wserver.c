2476b6fa5d984c14a343de234af48c8e1173f6bd
Cristian Muñoz <cristian.munoz3@udea.edu.co>
2025-06-06 21:20:47 -0500

    Se agrega concurrencia

--- lab03-webserver/wserver.c
+++ lab03-webserver/wserver.c
@@ -2,12 +2,22 @@
 #include "request.h"
 #include "io_helper.h"
 #include <pthread.h>
+#include "buffer.h"
 
 char default_root[] = ".";
 
 //
 // ./wserver [-d <basedir>] [-p <portnum>] 
-// 
+//
+void* consumer_function(void* arg){
+    while(1){
+        int conn_fd = buffer_remove(&request_buffer);
+        request_handle(conn_fd);
+        close_or_die(conn_fd);
+    }
+    return NULL;
+}
+
 int main(int argc, char *argv[]) {
     int c;
     char *root_dir = default_root;
@@ -34,19 +44,35 @@ int main(int argc, char *argv[]) {
 	    fprintf(stderr, "usage: wserver [-d basedir] [-p port] [-t number of threads] [-b buffers] [-s FIFO or SFF]\n");
         exit(1);
 	}
-
+    
     // run out of this directory
     chdir_or_die(root_dir);
 
     // now, get to work
     int listen_fd = open_listen_fd_or_die(port);
-    while (1) {
-	struct sockaddr_in client_addr;
-	int client_len = sizeof(client_addr);
-	int conn_fd = accept_or_die(listen_fd, (sockaddr_t *) &client_addr, (socklen_t *) &client_len);
-	request_handle(conn_fd);
-	close_or_die(conn_fd);
+
+    //initialize the buffer with #buffer spaces
+    buffer_init(&request_buffer, buffers);
+
+    // Create the threads pool
+    pthread_t workers[threads];
+
+    for(int i = 0; i < threads; i++){
+        int* p_listen_fd = malloc(sizeof(int));
+        *p_listen_fd = listen_fd;
+        pthread_create(&workers[i], NULL, consumer_function, NULL);
     }
+    
+    while (1) {
+	    struct sockaddr_in client_addr;
+	    int client_len = sizeof(client_addr);
+	    int conn_fd = accept_or_die(listen_fd, (sockaddr_t *) &client_addr, (socklen_t *) &client_len);
+
+        buffer_insert(&request_buffer, conn_fd);
+    }
+
+    buffer_destroy(&request_buffer);
+
     return 0;
 }
 
